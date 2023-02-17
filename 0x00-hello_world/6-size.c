7f24816c9d12bfd942e96aeedd394d38ee3cb2c2
ktonny <kalahannah2@gmail.com>
2023-02-16 18:44:03 -0800

    1-compiler

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,8 +7,9 @@ int main(void)
 	long int l;
 	long long ll;
 	float fl;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int:  %lu byte(s)\n", sizeof(i));
+	printf("Size of an int: %lu byte(s)\n", sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
 	printf("Size of a float: %lu byte(s)\n", sizeof(fl));
