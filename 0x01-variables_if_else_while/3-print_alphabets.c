69e93e6bbab96c85ab3d498693a9f58149324911
ktonny <kalahannah2@gmail.com>
2023-02-19 09:12:43 -0800

    print small the capital letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,17 @@
 int main(void)
 {
 	int i;
+	int n;
 
 	for (i = 97; i <= 122; i++)
 	{
-		putchar(c);
+		putchar(i);
 	}
+	for (n = 65; n <= 90; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
 	return (0);
 }
 
