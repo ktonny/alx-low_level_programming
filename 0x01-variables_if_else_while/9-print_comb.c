f8abd0893434d22a048eae79b59204922b6998f1
ktonny <kalahannah2@gmail.com>
2023-02-19 12:48:04 -0800

    dddd

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
   *main - Entry point
-  *
+  *prints all possible combination of singlre digit number.
   *Return - Always 0.
   */
 int main(void)
 {
- 	int n;
+	int n;
 
 	for (n = 48; n <= 57; n++)
 	{
