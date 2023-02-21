7b4c930219aede400ea1838f24cdc4911b09f73d
ktonny <kalahannah2@gmail.com>
2023-02-21 15:58:21 -0800

    prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <main.h>
+/**
+  *_islower - checks for lower character
+  *@c
+  *Return: 1 or 0
+  */
+int _islower(int c)
+{
+	if (c >= 97 || c >= 122)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
+
