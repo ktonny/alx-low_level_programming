4159566464e0e641f9ec1b2370b181abbd3beb1f
ktonny <kalahannah2@gmail.com>
2023-02-21 18:52:54 -0800

    prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+/**
+  *print_to_98 -  prints all natural numbers
+  *@n: integer
+  *Return: void
+  */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i);
+			_putchar(',');
+			if (i == 98)
+			{
+				break;
+			}
+			_putchar(' ');
+		}
+	}
+	else if (n == 98)
+	{
+		_putchar(n);
+	}
+	else if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i);
+			_putchar(',');
+			if (i == 98)
+			{
+				break;
+			}
+			_putchar(',')
+		}
+	}
+}
