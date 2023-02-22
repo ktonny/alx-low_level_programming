741c8053eaaa7849cded310db217708cefe79742
ktonny <kalahannah2@gmail.com>
2023-02-21 19:00:23 -0800

    prints alphabet in lowercase

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,30 +13,30 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			_putchar(i);
-			_putchar(',');
+			printf("%d", i);
+			printf(',');
 			if (i == 98)
 			{
 				break;
 			}
-			_putchar(' ');
+			printf(' ');
 		}
 	}
 	else if (n == 98)
 	{
-		_putchar(n);
+		printf("%d", n);
 	}
 	else if (n > 98)
 	{
 		for (i = n; i >= 98; i--)
 		{
-			_putchar(i);
-			_putchar(',');
+			printf("%d", i);
+			printf(',');
 			if (i == 98)
 			{
 				break;
 			}
-			_putchar(',');
+			printf(',');
 		}
 	}
 }
