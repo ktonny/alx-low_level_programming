8bd3f195e871554bee7dab0e5a3af4742d6fec27
ktonny <kalahannah2@gmail.com>
2023-02-21 17:01:01 -0800

    prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  *print_sign - prints the sign of a number
+  *@n: the number to be checked
+  *Return: 0, 1, -1
+  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
+
+}
