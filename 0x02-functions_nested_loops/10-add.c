4ff238a1e82fe3544a3760863dd12518fd5a84c2
ktonny <kalahannah2@gmail.com>
2023-02-21 18:18:42 -0800

    prints alphabet in lowercase

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  *add - adds two numbers
+  *@a: integer
+  *@b: integer
+  *Return: results
+  */
+int add(int a, int b)
+{
+	int a;
+	int b;
+	int results;
+
+	results = a + b;
+	return (results);
+}
