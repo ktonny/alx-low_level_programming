5a8ec7cbd7351d92da43edeee3307e43be8849d8
ktonny <kalahannah2@gmail.com>
2023-02-23 14:20:37 -0800

    prints the numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main"
+/**
+  *print_most_numbers -  prints the numbers, from 0 to 9
+  *
+  *Return: void
+  */
+void print_most_numbers(void)
+{
+	int c;
+	for (c = 48; c <= 57; c++)
+	{
+		if (c = 50 || c = 52)
+		{
+			continue;
+		}
+		_putchar(c);
+	}
+	_putchar('\n');
+}
