30027d4d11f823c269d54f2de3be3683ade93b9c
ktonny <kalahannah2@gmail.com>
2023-02-23 06:08:05 -0800

    checks if letter is uppercase

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *_isdigit - checks for a digit
+  *@c: number to e checked
+  *Return: 0 0r 1
+  */
+int _isdigit(int c)
+{
+	if (c  >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
