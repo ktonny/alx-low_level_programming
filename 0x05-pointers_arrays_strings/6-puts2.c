04c361b7c1ee99d6f17ab51168596aa88e0417b1
ktonny <kalahannah2@gmail.com>
2023-02-28 11:20:00 -0800

    print string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+  *puts2 - prints every character of a string
+  *@str: string
+  *Return: void
+  */
+void puts2(char *str)
+{
+	int u;
+	int i;
+
+	u = 0;
+	while (str[u] != '\0')
+	{
+		u++;
+	}
+	for (i = 0; i <= u - 1; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
