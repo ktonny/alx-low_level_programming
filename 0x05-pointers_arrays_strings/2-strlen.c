3b604dd156ac1cac4e2ee6794c54ee0f42d6237a
ktonny <kalahannah2@gmail.com>
2023-02-27 16:21:16 -0800

    function that swaps two integers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *_strlen - returns the length of a string.
+  *@s: string
+  *Return: 0;
+  */
+int _strlen(char *s)
+{
+	int u;
+
+	u = 0;
+	while(s[u] = '/0')
+	{
+		u++;
+	}
+	return u;
+}
