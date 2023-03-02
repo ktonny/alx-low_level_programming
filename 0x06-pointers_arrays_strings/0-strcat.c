a9aef16cece636a2c67630ceb358ca5384d7dd39
ktonny <kalahannah2@gmail.com>
2023-03-02 14:59:07 -0800

    string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -12,16 +12,16 @@ char *_strcat(char *dest, char *src)
 	int n;
 
 	i = 0;
-	while(dest[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 	m = 0;
-	while(src[m] != '\0')
+	while (src[m] != '\0')
 	{
 		m++;
 	}
-	for(n = 0; n < m; n++)
+	for (n = 0; n < m; n++)
 	{
 		dest[i] = src[n];
 		i++;
