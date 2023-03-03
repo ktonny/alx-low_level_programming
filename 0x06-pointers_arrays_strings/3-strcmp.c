4be62601e43fa9bb24b7611d24bb635d530d1dd3
ktonny <kalahannah2@gmail.com>
2023-03-03 00:07:12 -0800

    compare string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-	int flag;
+	int flag = 0;
 	
 	i = 0;
 	while (s1[i] != '\0' || s2[i] != '\0')
@@ -18,6 +18,7 @@ int _strcmp(char *s1, char *s2)
 			flag = s1[i] - s2[i];
 			break;
 		}
+	i++;
 	}
 	return (flag);
 }
