198c6a7f0901a97f6911078dc129b6b8e220104b
ktonny <kalahannah2@gmail.com>
2023-03-03 02:02:09 -0800

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,13 +3,13 @@
   *_strcmp - compares two strings
   *@s1: point to a string to be changed
   *@s2: points to a string to be changed
-  *Return: int
+  *Return: flag
   */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
 	int flag;
-	
+
 	i = 0;
 	flag = 0;
 	while (s1[i] != '\0' || s2[i] != '\0')
