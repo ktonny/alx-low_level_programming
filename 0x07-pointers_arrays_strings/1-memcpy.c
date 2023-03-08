8cb06e938c7585dc199d918cf4d7fb2d75a126e4
ktonny <kalahannah2@gmail.com>
2023-03-07 18:09:07 -0800

    mem copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,8 +3,9 @@
 /**
   *_memcpy - copies memory area
   *@dest: dest memory area
-  *src: source
-  *n: number of bytes
+  *@src: source
+  *@n: number of bytes
+  *Return: dest
   */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
@@ -12,11 +13,11 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	if (dest == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	return dest;
+	return (dest);
 }
